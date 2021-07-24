d4664606d413717072042fb8488568e572b9925f
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-23 22:24:21 -0500

    Organizando rutas en archivo aparte

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 // Definiendo puerto
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-	res.send('Probando funcionamiento');
-})
+// Agregando router
+app.use('/', router);
 
 app.listen(port, () => {
 	console.log(`El servidor esta funcionando en el puerto ${port}`)
