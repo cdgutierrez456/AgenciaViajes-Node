12afd9b54dc6861fefca90c3386b40b6616bf617
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-25 20:59:22 -0500

    Conectando base de datos

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+import {pass} from './pass.js';
+
+const db = new Sequelize('agenciaviajes', 'root', `${pass}`, {
+    host: '127.0.0.1',
+    port: '3306',
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    operatorAliases: false
+});
+
+export default db;
+
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+// Conectando la base de datos
+db.authenticate()
+	.then(() => console.log('Base de datos conectada'))
+	.catch(error => console.log(error));
 
 // Definiendo puerto
 const port = process.env.PORT || 4000;
